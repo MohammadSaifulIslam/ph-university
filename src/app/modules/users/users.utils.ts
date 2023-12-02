65b0ada94202a1d40d4af34662bfc822f907d4e3
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-12-02 09:53:03 +0600

    genarate student id unique for different semester and yser bug fix

--- src/app/modules/users/users.utils.ts
+++ src/app/modules/users/users.utils.ts
@@ -7,10 +7,23 @@ const findLastUserId = async () => {
   const result = await User.findOne({ role: 'student' }, { id: 1, _id: 0 })
     .sort({ createdAt: -1 })
     .lean();
-  return result?.id ? result.id.substring(6) : undefined;
+  return result?.id ? result.id : undefined;
 };
 export const genarateId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastUserId()) || (0).toString();
+  let currentId = (0).toString();
+  // id = 2030 01 0001
+  const lastStudentId = await findLastUserId();
+  const lastStudentSemesterYear = lastStudentId?.substring(0, 4);
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const currentSemesterYear = payload.year;
+  const currentSemesterCode = payload.code;
+  if (
+    lastStudentId &&
+    lastStudentSemesterYear === currentSemesterYear &&
+    lastStudentSemesterCode === currentSemesterCode
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
   let increamentId = (Number(currentId) + 1).toString().padStart(4, '0');
 
   return (increamentId = `${payload.year}${payload.code}${increamentId}`);
