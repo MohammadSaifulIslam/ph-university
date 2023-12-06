99491a11226398f012767d28557660b0b0138058
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-12-06 20:26:42 +0600

    made custom error formet for zod error

--- src/app/middlewers/globalErrorHandler.ts
+++ src/app/middlewers/globalErrorHandler.ts
@@ -1,21 +1,49 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  // eslint-disable-next-line no-unused-vars
-  next: NextFunction,
-) => {
-  const statusCode = err.status || 500;
-  const message = err.message || 'Someting went wrong!';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = err.status || 500;
+  let message = err.message || 'Someting went wrong!';
+
+  type TErrorSource = {
+    path: string | number;
+    message: string;
+  }[];
+
+  let errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'Someting went wrong!',
+    },
+  ];
+
+  const zodErrorHandler = (err: ZodError) => {
+    return {
+      statusCode: 400,
+      message: 'Validation error',
+      errorSource: err?.issues?.map((issue) => {
+        return {
+          path: issue?.path[issue?.path?.length - 1],
+          message: issue?.message,
+        };
+      }),
+    };
+  };
+
+  if (err instanceof ZodError) {
+    const simplifiedError = zodErrorHandler(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSource = simplifiedError.errorSource;
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
-    err,
+    errorSource,
   });
 };
 
